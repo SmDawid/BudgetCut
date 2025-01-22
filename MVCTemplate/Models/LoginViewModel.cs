133ac66467baeeb224ac052ad9258aa839c1b90c
dsmylaWSB <wiseguyfrompoland@gmail.com>
2025-01-22 14:18:18 +0100

    wrzutka1.5

---
+++ MVCTemplate/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCTemplate.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+
+        public DateTime LoginDateTime { get; set; } = DateTime.Now;
+    }
+}
--- MVCTemplate/Program.cs
+++ MVCTemplate/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MVCTemplate.Data;
+using MVCTemplate.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
